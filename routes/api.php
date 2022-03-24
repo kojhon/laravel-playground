7a62a70828c66fb38f1b048eb2bcd642718d1a47
Evgeniy Kovrizhnikov <ekovrizhnikov@plesk.com>
2022-03-24 14:34:51 +0700

    Add some routes to test clean URLs support

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/test', function (Request $request) {
+    return [
+        'headers' => $request->headers->all(),
+        'data' => $request->all(),
+        'query' => $request->query->all(),
+    ];
+});
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    return view('test');
+});
+
+Route::get('/redirect', function () {
+    return redirect('/test');
+});
+
+Route::get('/testError', function () {
+    abort(403, 'Unauthorized action.');
+});
